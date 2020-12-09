f0c5e87d7a70e9a94f78e78ad6ee39a5f144c7d1
Tom Kelley <tom@tomkel.com>
2020-12-08 22:03:01 -0500

    Begin Advent of Code project

---
+++ src/main/java/com/tomkel/advent/advent2020/App.java
@@ -0,0 +1,13 @@
+package com.tomkel.advent.advent2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
