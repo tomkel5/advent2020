2a7a10a290f84e560f30667e29b6a4d55b488e21
Tom Kelley <tom@tomkel.com>
2020-12-16 17:22:01 -0500

    Day 14, part 2

--- src/main/java/com/tomkel/advent/advent2020/day14/MaskUtils.java
+++ src/main/java/com/tomkel/advent/advent2020/day14/MaskUtils.java
@@ -1,5 +1,8 @@
 package com.tomkel.advent.advent2020.day14;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaskUtils {
 
     public static long fromString(String bits) {
@@ -45,5 +48,52 @@ public class MaskUtils {
         }
 
         return fromString(result.toString());
+    }
+
+
+    /**
+     * Create an extended mask by applying the original mask to the number, keeping the floating bits intact.
+     * Example:
+     *   Number:        101010
+     *   Mask:          X1001X
+     *   Extended Mask: X1101X
+     * Finding all 1/0 combinations of floating bits will give us all possible permutations of this mask.
+     */
+    public static String extendMask(long number, String mask) {
+
+        String n = fromLong(number);
+        StringBuilder result = new StringBuilder(36);
+
+        for(int i = 0; i < 36; i ++) {
+            if (mask.charAt(i) == 'X') {
+                result.append('X');
+            } else if (mask.charAt(i) == '1' || n.charAt(i) == '1') {
+                result.append('1');
+            } else {
+                result.append('0');
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Find all permutations of this extended mask by recursively change floating bits to 1s and 0s.
+     */
+    private static List<String> getMaskPermutations(String mask, List<String> running) {
+        int i = mask.indexOf('X');
+
+        if (i >= 0)  {
+            running.addAll(getMaskPermutations(mask.replaceFirst("X", "1")));
+            running.addAll(getMaskPermutations(mask.replaceFirst("X", "0")));
+        } else {
+            running.add(mask);
+        }
+
+        return running;
+    }
+
+    public static List<String> getMaskPermutations(String mask) {
+        return getMaskPermutations(mask, new ArrayList<>());
     }
 }
---
+++ src/main/java/com/tomkel/advent/advent2020/day14/Part2.java
@@ -0,0 +1,54 @@
+package com.tomkel.advent.advent2020.day14;
+
+import com.tomkel.advent.advent2020.FileHelpers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Part2 {
+
+    /**
+     * @see <a href="https://adventofcode.com/2020/day/14#part2">Puzzle 14.2</a>
+     */
+    public static void main(String[] args) throws Exception {
+        List<String> lines = FileHelpers.getLines("day14/input.txt");
+
+        Map<Long, Long> memory = new HashMap<>();
+
+        // Start with a no-op a mask (it should get overwritten by the first line anyway).
+        String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        for (String line : lines) {
+            // Example format: "mask = 1X101010X01101110000001110X11XXX0100"
+            if (line.startsWith("mask =")) {
+
+                String[] parts = line.split(" ");
+
+                // Change the mask:
+                mask = parts[2];
+            }
+
+            else if (line.startsWith("mem")) {
+                String[] parts = line.split(" ");
+                long argument = Long.parseLong(parts[2]);
+                long index = Long.parseLong(parts[0].replace("mem[", "").replace("]", ""));
+
+                // Create an extended mask that masks the existing mask over the number.
+                String extendedMask = MaskUtils.extendMask(index, mask);
+
+                List<String> permutations = MaskUtils.getMaskPermutations(extendedMask);
+                permutations.forEach(p -> {
+                    // Each permutation of the mask at this stage will be strictly 0s and 1s.
+                    // As a matter of convenience, we can use applyMask() to mask over any long value.
+                    long key = MaskUtils.applyMask(index, p);
+                    memory.put(key, argument);
+                });
+            }
+        }
+
+        long sum = memory.values().stream().mapToLong(l -> l).sum();
+
+        System.out.printf("The sum is: %s%n", sum);
+    }
+}
